b1aa17af81fabe78adc2443edccae504bdeba8fd
Wilmerlop733 <wilmerlopezjp@gmail.com>
2026-02-10 15:58:28 -0600

    Cambio en diseño del frmBienvenido

--- Restaurante1/frmBienvenido.Designer.cs
+++ Restaurante1/frmBienvenido.Designer.cs
@@ -19,36 +19,41 @@ namespace Restaurante1
             lblWelcome = new Label();
             lblSubtitle = new Label();
             SuspendLayout();
-
+            // 
+            // lblWelcome
+            // 
             lblWelcome.AutoSize = true;
             lblWelcome.Font = new Font("Segoe UI", 24F, FontStyle.Bold);
             lblWelcome.ForeColor = Color.FromArgb(40, 50, 60);
-            lblWelcome.Location = new Point(171, 267);
+            lblWelcome.Location = new Point(245, 341);
             lblWelcome.Name = "lblWelcome";
             lblWelcome.Size = new Size(649, 54);
             lblWelcome.TabIndex = 0;
             lblWelcome.Text = "Bienvenido al Sistema de Gestión";
             lblWelcome.TextAlign = ContentAlignment.MiddleCenter;
-
+            // 
+            // lblSubtitle
+            // 
             lblSubtitle.AutoSize = true;
             lblSubtitle.Font = new Font("Segoe UI", 14F);
             lblSubtitle.ForeColor = Color.Gray;
-            lblSubtitle.Location = new Point(229, 347);
+            lblSubtitle.Location = new Point(266, 402);
             lblSubtitle.Name = "lblSubtitle";
             lblSubtitle.Size = new Size(600, 32);
             lblSubtitle.TabIndex = 1;
             lblSubtitle.Text = "Seleccione una opción del menú lateral para comenzar";
             lblSubtitle.TextAlign = ContentAlignment.MiddleCenter;
-
-
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            // 
+            // frmBienvenido
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1029, 800);
+            ClientSize = new Size(1176, 1067);
             Controls.Add(lblSubtitle);
             Controls.Add(lblWelcome);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 4, 3, 4);
-            Name = "WelcomeForm";
+            Margin = new Padding(3, 5, 3, 5);
+            Name = "frmBienvenido";
             Text = "Bienvenida";
             ResumeLayout(false);
             PerformLayout();
